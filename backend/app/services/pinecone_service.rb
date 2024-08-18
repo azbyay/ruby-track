adef9bea29927efdc3a8a392c2c876a7e25864d3
Muhammad Umar Amir <125129208+M-Umar-Amir@users.noreply.github.com>
2024-08-18 14:39:05 +0000

    working pinecone

--- backend/app/services/pinecone_service.rb
+++ backend/app/services/pinecone_service.rb
@@ -3,12 +3,13 @@ require 'http'
 
 class PineconeService
   PINECONE_BASE_URL = "https://ruby-track-8abi2f4.svc.aped-4627-b74a.pinecone.io" # Replace with your Pinecone URL
-  PINECONE_API_KEY = ENV[PINECONE_API_KEY]
+  PINECONE_API_KEY = '98ce733e-3b52-4733-8be0-3216331473f9'
 
   def initialize
     @headers = {
       "Content-Type" => "application/json",
-      "Api-Key" => PINECONE_API_KEY
+      "Api-Key" => PINECONE_API_KEY,
+      'X-Pinecone-API-Version' => "2024-07",
     }
   end
 
@@ -20,9 +21,9 @@ class PineconeService
       "includeMetadata" => true
     }
 
-    response = HTTP.headers(@headers).post("#{PINECONE_BASE_URL}/vectors/query", json: payload)
+    response = HTTP.headers(@headers).post("#{PINECONE_BASE_URL}/query", json: payload)
 
-    Rails.logger.debug("#{@headers}")
+    Rails.logger.debug("#{response}")
 
 
     if response.status.success?
